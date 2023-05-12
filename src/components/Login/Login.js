a1418ab8992d14f148c77f48ac824bcf1da2123b
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-05-12 14:32:19 +0530

    style task7

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -9,76 +9,101 @@ const Login = (props) => {
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
+  const [enteredCollegeName, setEnteredCollegeName] = useState('');
   const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+
+    setFormIsValid(
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredCollegeName.trim() !== ''
+    );
+  }, [enteredEmail, enteredPassword, enteredCollegeName]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
-  };
+  }
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+      setEnteredPassword(event.target.value);
+    };
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    const collegeNameChangeHandler = (event) => {
+      setEnteredCollegeName(event.target.value);
+    };
+
+    const validateEmailHandler = () => {
+      setEmailIsValid(enteredEmail.includes('@'));
+    };
+
+    const validatePasswordHandler = () => {
+      setPasswordIsValid(enteredPassword.trim().length > 6);
+    };
+
+    const validateCollegeNameHandler = () => {
+      setFormIsValid(
+        enteredEmail.includes('@') &&
+        enteredPassword.trim().length > 6 &&
+        enteredCollegeName.trim() !== ''
+      );
+    };
+
+    const submitHandler = (event) => {
+      event.preventDefault();
+      props.onLogin(enteredEmail, enteredPassword);
+    };
+
+    return (
+      <Card className={classes.login}>
+        <form onSubmit={submitHandler}>
+          <div
+            className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+              }`}
+          >
+            <label htmlFor="email">E-Mail</label>
+            <input
+              type="email"
+              id="email"
+              value={enteredEmail}
+              onChange={emailChangeHandler}
+              onBlur={validateEmailHandler}
+            />
+          </div>
+          <div
+            className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+              }`}
+          >
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={enteredPassword}
+              onChange={passwordChangeHandler}
+              onBlur={validatePasswordHandler}
+            />
+          </div>
+          <div
+          className={`${classes.control} ${
+            formIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="collegeName">College Name</label>
+          <input
+            type="text"
+            id="collegeName"
+            value={enteredCollegeName}
+            onChange={collegeNameChangeHandler}
+            onBlur={validateCollegeNameHandler}
+          />
+        </div>
+          <div className={classes.actions}>
+            <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+              Login
+            </Button>
+          </div>
+        </form>
+      </Card>
     );
   };
 
-  const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
-  };
 
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
-  };
-
-  return (
-    <Card className={classes.login}>
-      <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
-        <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
-            Login
-          </Button>
-        </div>
-      </form>
-    </Card>
-  );
-};
-
-export default Login;
+  export default Login;
